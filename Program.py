283de83b2a7e6b7c59ecc79422a3ef4b1ff5f5d3
Rohan Somadder <87234603+Rohan-Somadder@users.noreply.github.com>
2021-08-30 18:27:24 +0530

    Updated Plans and proper structure and naming

--- Program.py
+++ Program.py
@@ -1,7 +1,10 @@
 import random
 from getch import pause
 
-class user:
+choices = ['rock','paper','scissors']
+
+
+class User:
     def __init__(self):
         self.name=input("\nEnter your name : ")
         self.points = 0
@@ -12,7 +15,6 @@ class user:
     def __str__(self):
         return "\nYour score is: "+ str(self.points)
 
-choices = ['rock','paper','scissors']
 
 def Game_rules():
     print('\nA player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors"),\
@@ -20,7 +22,8 @@ def Game_rules():
          \nIf both players choose the same shape, the game is tied and is usually immediately replayed to break the tie.')
     pass
 
-def win_or_lose(p_ch):                  #returns win ,tie or lose
+
+def Win_or_Lose(p_ch):  # Returns win ,tie or lose
     com_ch = choices[random.randint(0,2)]
     if p_ch == com_ch :
         return 1
@@ -39,15 +42,16 @@ def win_or_lose(p_ch):                  #returns win ,tie or lose
             return 0
         elif p_ch == 'rock':
             return 2
-    
-def play():
-    u=user()
+
+
+def Play():
+    u=User()
     play = True
-    menu()
+    Menu()
     while play:
         choice = input("\nEnter your choice (Rock/Paper/Scissors) : ")
         if choice.lower() in choices:
-            result = win_or_lose(choice)
+            result = Win_or_Lose(choice)
             if result == 2:
                 print('\nMatch Won')
                 u.update_points() 
@@ -57,28 +61,35 @@ def play():
                 print('\nMatch Loose')
         else:
             print("\nYou have given a wrong input as a choice")
-            play = redo()
+            play = Redo()
             continue
         print(u)
-        if not redo() :
+        if not Redo() :
             play = False
 
-def menu():
+
+def Menu():
     rules = input("\nDo you want to read the rules? (Y/N) : ")
     if rules == 'Y' or rules == 'y':
         Game_rules()
 
-def redo():
-    
+
+def Redo(): 
     c=input("\nDo you want to play again? (Y/N) :")
     if c == 'Y' or c =='y':
         return True
     else:
         return False
 
+
+def FileIO():  # To include the file inputs in a csv file for ML and record keeping
+    pass  # TODO: Make 2 files one with all records and another with 10 highest scores 
+          # (Also after achieving high score ask to include and not if not selected)
+
+
 if __name__=="__main__":
     print('\n----------------------------------------------------------------------------------------------------------')
     print('\nWelcome to the game of rock paper and scissors')
-    play()
+    Play()
     print('\n----------------------------------------------------------------------------------------------------------')
     pause("\n\nGame Finished!!! \n\nPress any key to exit....")
